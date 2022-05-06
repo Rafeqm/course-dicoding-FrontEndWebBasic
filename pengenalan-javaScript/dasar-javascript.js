54e736f2bc4285ebf63507ca791976dbc12ad1cc
Muhammad Rafiqi <87463084+Rafeqm@users.noreply.github.com>
2022-05-06 16:21:24 +0700

    add(update): JavaScript Comment

--- pengenalan-javaScript/dasar-javascript.js
+++ pengenalan-javaScript/dasar-javascript.js
@@ -1,1 +1,10 @@
 alert("Terima kasih.");
+
+// ini merupakan komentar satu baris komentar
+
+// alert("Terima kasih")
+
+/* Ini merupakan komentar dengan lebih dari satu baris
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
