d970c49b28bf2cc5bb00c66abe4e0bdd669cd9c3
Muhammad Rafiqi <87463084+Rafeqm@users.noreply.github.com>
2022-05-06 16:40:07 +0700

    add(update): JavaScript Variable

--- pengenalan-javaScript/dasar-javascript.js
+++ pengenalan-javaScript/dasar-javascript.js
@@ -8,3 +8,26 @@ alert("Terima kasih.");
 Teks apapun yang berada disini akan dijadikan komentar.
 Ketika menggunakan ini, jangan lupa untuk menutupnya.
 */
+
+var firstName = "Mya";
+console.log(firstName);
+
+firstName = "Alda";
+console.log(firstName);
+
+// The controversial hoisting when using the 'var' keyword
+x = 100;
+var x;
+console.log(x);
+
+// Always use the 'let' keyword instead
+y = 100;
+let y;
+console.log(y); // ReferenceError: Cannot access 'y' before initialization
+
+// This is a 'const'ant variable, so it can't be changed/reassigned
+const z = 100;
+console.log(z);
+
+z = 200; // TypeError: Assignment to constant variable.
+console.log(z);
