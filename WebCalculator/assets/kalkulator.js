e7646615972ba1ec296e595744d2e4bb5a72af87
Muhammad Rafiqi <87463084+Rafeqm@users.noreply.github.com>
2022-05-06 19:40:41 +0700

    fix: Mendapatkan Elemen

--- WebCalculator/assets/kalkulator.js
+++ WebCalculator/assets/kalkulator.js
@@ -1,1 +1,9 @@
 console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+const head = document.head;
+const body = document.body;
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  console.log(button);
+}
