23edfc0f692c171e6c6129f0cbc0c44f6ef4f780
Muhammad Rafiqi <87463084+Rafeqm@users.noreply.github.com>
2022-05-06 21:42:22 +0700

    fix: Menerapkan JavaScript pada Web Kalkulator

--- WebCalculator/assets/kalkulator.js
+++ WebCalculator/assets/kalkulator.js
@@ -1,9 +1,94 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+const calculator = {
+  displayNumber: "0",
+  firstNumber: null,
+  operator: null,
+  waitingForSecondNumber: false
+};
 
-const head = document.head;
-const body = document.body;
+function updateDisplay() {
+  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearDisplay() {
+  calculator.displayNumber = "0";
+  calculator.firstNumber = null;
+  calculator.operator = null;
+  calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+  if (calculator.displayNumber === "0") {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
+}
+
+function inverseNumber() {
+  if (calculator.displayNumber === "0") {
+    return;
+  }
+  calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+  if (!calculator.waitingForSecondNumber) {
+    calculator.operator = operator;
+    calculator.firstNumber = calculator.displayNumber;
+    calculator.waitingForSecondNumber = true;
+
+    // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+    calculator.displayNumber = "0";
+  } else {
+    alert("Operator sudah ditetapkan");
+  }
+}
+
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === "+") {
+    result =
+      parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result =
+      parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+
+  calculator.displayNumber = result;
+}
+
 const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+  button.addEventListener("click", function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
 
-for (const button of buttons) {
-  console.log(button);
+    if (target.classList.contains("clear")) {
+      clearDisplay();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains("operator")) {
+      handleOperator(target.innerText);
+      return;
+    }
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
 }
